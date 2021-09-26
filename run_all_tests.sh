2b0116b2a56b9df62bc272bc7ceca7245c613421
werner <werner.dittmann@t-online.de>
2021-09-26 09:48:58 +0200

    Upgrade to use newer Android API version, add option to build with tests

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,13 +1,15 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 dir=cmake-build-debug
 
 for tst_dir in P434 P503 P610 P751; do
   for tst in ${dir}/${tst_dir}/*_test; do
-    echo running ${tst}
+    printf "running ${tst} "
     ./${tst} >/dev/null
     if test ! $?; then
-      echo ${tst} failed.
+      printf "... FAILED.\n"
+    else
+      printf "... PASSED.\n"
     fi
   done
 done
